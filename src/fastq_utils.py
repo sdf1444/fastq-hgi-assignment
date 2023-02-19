daa8715649dc394d1eed04420808dd11da483ec1
Spencer Du <spencer@wearelighten.co.uk>
2023-02-19 06:36:53 +0000

    Count the number of lines that start with "@" (which indicate the start of a new sequence)

--- src/fastq_utils.py
+++ src/fastq_utils.py
@@ -5,3 +5,6 @@ def sequence_count(file):
   with open(file) as f:
     # Read the contents of the file
     file_contents = f.read()
+  # Count the number of lines that start with "@" (which indicate the start of a new sequence)
+  num_sequences = len([line for line in file_contents.split("\n") if line.strip() != ""]) // 4
+  return num_sequences
